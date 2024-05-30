dc4cb0c3f464d69220539a1d64c5fb0d250ec82b
Adiallo123 <aissatoudiallobd@gmail.com>
2024-05-30 17:38:43 +0200

    feat: create a file createTodo and add a function createTodo

---
+++ frontend/assets/script/createTodo.mjs
@@ -0,0 +1,18 @@
+export default function createTodo(container) {
+    const div = document.createElement("div");
+    container.append(div);
+    const inputCheck = document.createElement("input");
+    inputCheck.type = "checkbox";
+    div.appendChild(inputCheck);
+    const label = document.createElement("label");
+    label.textContent = "New Todo";
+    label.contentEditable = true;
+    div.appendChild(label);
+    label.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            label.blur();
+        }
+    });
+    label.focus();
+
+}
