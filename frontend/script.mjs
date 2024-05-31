a94ad6a9c80a9ad7f9e12e8f4f14e63965a5822d
Adiallo123 <aissatoudiallobd@gmail.com>
2024-05-31 10:57:03 +0200

    resolve error index.html

--- frontend/script.mjs
+++ frontend/script.mjs
@@ -1,3 +1,12 @@
 import createCard from "./assets/script/createCard.mjs";
+import createTodo from "./assets/script/createTodo.mjs";
 
 createCard("Groceries");
+//createTodo();
+const container = document.querySelector(".card > main > .left")
+
+const img = document.querySelector("img")
+
+img.addEventListener('click', () => {
+    createTodo(container);
+});
